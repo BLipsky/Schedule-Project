9d9b4594b64671ca67e1978055711fafe8b061ef
BLipsky <BenLipsky@icloud.com>
2024-10-30 10:43:56 -0400

    g

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const bellSchedule = {
     4: { start: '12:41 PM', end: '1:48 PM' },
     5: { start: '1:53 PM', end: '3:00 PM' }
 };
+// Filter didn't work so instead use this to make sure correct periods appear right for each day 
 const dailyPeriods = {
     A: [1, 2, 3, "Lunch", 5, 6],
     B: [4, 1, 2, "Lunch", 7, 5],
@@ -18,17 +19,20 @@ const dailyPeriods = {
     F: [4, 1, 2, "Lunch", 6, 7],
     G: [3, 4, 7, "Lunch", 5, 6]
 };
+// Select Day from Dropdown
 btn.on("click", () => {
-    var selectedDay = $('#dayInput').val().toUpperCase();
+    var selectedDay = $('#dayInput').val();
     $.ajax({
       url: `https://api.npoint.io/22bb26e45d4697fb3609`,
       method: "GET",
       success: (data) => {
+// Access jSon and pull each individual class and put it to the write day
         const schedule = data.schedule; 
         const daySchedule = dailyPeriods[selectedDay];
         $('#scheduleList').empty();
         let bellIndex = 1;
         daySchedule.forEach((period) => {
+// Create and display constant "lunchtime" element (always 4th box)
           if (period === "Lunch") {
             const lunchTime = bellSchedule.lunch;
             $('#scheduleList').append(`
@@ -39,11 +43,13 @@ btn.on("click", () => {
               </tr>
             `);
           } else {
+//function to display every other period - pull info from jSon and find which period loops through each day
             const periodData = schedule.find(item => item.period === period && item.days.includes(selectedDay));
             if (periodData) {
+// Create every other block 
               const time = bellSchedule[bellIndex];
               $('#scheduleList').append(`
-                <tr>
+                <tr class="centerall">
                   <td>${period}</td>
                   <td>${time.start} - ${time.end}</td>
                   <td>${periodData.class}</td>
@@ -56,6 +62,7 @@ btn.on("click", () => {
           }
         });
       },
+// Error message if all else fails
       error: () => {
         console.log("We connected to the server, but it returned an error.");
       },
